74b30018a1d767ed748d55b8f6f11ca38e37e894
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-23 19:00:47 -0300

    implementando estrutura inicial para o header da aplicacao

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
-  imports: [],
-  providers: [],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule
+  ],
+  exports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule
+  ],
 })
 export class MaterialModule {}
