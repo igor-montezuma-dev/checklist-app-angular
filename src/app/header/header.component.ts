e1f2d0da4b10eba2f0a6f18b8264d59b44160c77
IgorMontezuma20 <igorminranda17@gmail.com>
2023-09-24 15:08:32 -0300

    implementando o sideNavBar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  public openSideNav() {}
+  @Input() sidenav!: MatSidenav;
+
+  public openSideNav() {
+    this.sidenav.toggle();
+  }
 }
